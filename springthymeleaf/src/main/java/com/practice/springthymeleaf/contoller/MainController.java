fa4a4a17871803d056b6dbb82bc5a8a47cb50731
kpidiba <kpidibadavid1@gmail.com>
2023-02-01 18:44:44 +0000

    finish templating ,basic thymeleaf directives

--- springthymeleaf/src/main/java/com/practice/springthymeleaf/contoller/MainController.java
+++ springthymeleaf/src/main/java/com/practice/springthymeleaf/contoller/MainController.java
@@ -1,37 +1,47 @@
 package com.practice.springthymeleaf.contoller;
 
 import java.util.List;
-
 import org.springframework.stereotype.Controller;
-//NOTE:import
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 
 @Controller
 public class MainController {
-    
+
     @GetMapping("/home")
-    public String index(Model model){
+    public String index(Model model) {
         model.addAttribute("hello", "hello");
         return "index";
     }
 
     @GetMapping("/iteration")
-    public String iteratioString( Model model) {
-        List<String> tab = List.of("david","helloworld","man with a mission");
+    public String iteratioString(Model model) {
+        List<String> tab = List.of("david", "helloworld", "man with a mission");
         model.addAttribute("tab", tab);
         return "iterate";
     }
 
     @GetMapping("/handleCondition")
-    public String condition(Model model){
+    public String condition(Model model) {
         model.addAttribute("value", false);
         model.addAttribute("gender", "Masculin");
-        List<Integer> tab = List.of(123,344,544);
-        model.addAttribute("tableau",tab);
+        List<Integer> tab = List.of(123, 344, 544);
+        model.addAttribute("tableau", tab);
         return "condition";
     }
-    
+
+    //handler for including fragment(include,replace,insert)
+    @GetMapping("/service")
+    public String serviceHandler(Model m){
+        m.addAttribute("title", "I am Optimus Prime ");
+        m.addAttribute("subTitle", "SubTitle Omega Demonos");
+        return "service";
+    }
+
+    //templation
+    @GetMapping("/services")
+    public String templateHandler(){
+        return "templat";
+    }
+
 }
