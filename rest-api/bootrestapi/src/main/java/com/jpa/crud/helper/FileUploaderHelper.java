4d759462416d2ae305f4331bc809a85cea36b2d8
dprogrammer <kpidibadavid1@gmail.comm>
2023-08-14 12:02:23 +0000

    deploy

--- rest-api/bootrestapi/src/main/java/com/jpa/crud/helper/FileUploaderHelper.java
+++ rest-api/bootrestapi/src/main/java/com/jpa/crud/helper/FileUploaderHelper.java
@@ -11,40 +11,40 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
-@Component
-public class FileUploaderHelper {
-    // public final String UPLOAD_DIR = "C:\\Users\\kpidi\\Coding\\Github\\SPRING-BOOT\\bootrestapi\\src\\main\\resources\\static\\image";
-    public final String UPLOAD_DIR =  new ClassPathResource("static/image").getFile().getAbsolutePath();
+// @Component
+// public class FileUploaderHelper {
+//     // public final String UPLOAD_DIR = "C:\\Users\\kpidi\\Coding\\Github\\SPRING-BOOT\\bootrestapi\\src\\main\\resources\\static\\image";
+//     public final String UPLOAD_DIR =  new ClassPathResource("static/image").getFile().getAbsolutePath();
 
-    public FileUploaderHelper() throws Exception{
+//     public FileUploaderHelper() throws Exception{
         
-    }
+//     }
 
 
-    public boolean uploadFile(MultipartFile file){
-        boolean f= false;
-        try {
+//     public boolean uploadFile(MultipartFile file){
+//         boolean f= false;
+//         try {
 
-            //NOTE: method 1
-            InputStream is = file.getInputStream();
-            byte data[] = new byte[is.available()];
-            is.read(data);
+//             //NOTE: method 1
+//             InputStream is = file.getInputStream();
+//             byte data[] = new byte[is.available()];
+//             is.read(data);
 
-            //write
-            FileOutputStream fos = new FileOutputStream(UPLOAD_DIR+File.separator+file.getOriginalFilename());
-            fos.write(data);
-            fos.flush();
-            fos.close();
+//             //write
+//             FileOutputStream fos = new FileOutputStream(UPLOAD_DIR+File.separator+file.getOriginalFilename());
+//             fos.write(data);
+//             fos.flush();
+//             fos.close();
 
-            //NOTE:methode 2
-            Files.copy(file.getInputStream(),Paths.get( UPLOAD_DIR+File.separator+file.getOriginalFilename()), StandardCopyOption.REPLACE_EXISTING);
-            f=true;
-        } catch (Exception e) {
-            // TODO: handle exception
-            e.printStackTrace();
-        }
+//             //NOTE:methode 2
+//             Files.copy(file.getInputStream(),Paths.get( UPLOAD_DIR+File.separator+file.getOriginalFilename()), StandardCopyOption.REPLACE_EXISTING);
+//             f=true;
+//         } catch (Exception e) {
+//             // TODO: handle exception
+//             e.printStackTrace();
+//         }
 
-        return f;
-    }
+//         return f;
+//     }
 
-}
+// }
