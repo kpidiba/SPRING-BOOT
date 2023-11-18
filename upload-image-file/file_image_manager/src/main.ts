c693a651212f0031f809eeb204292f50b43ca66b
dprogrammer <kpidibadavid1@gmail.comm>
2023-11-18 16:12:16 +0000

    upload image dashboard - mis en place

--- upload-image-file/file_image_manager/src/main.ts
+++ upload-image-file/file_image_manager/src/main.ts
@@ -1,7 +1,29 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { Routes, provideRouter } from '@angular/router';
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+const routes: Routes = [
+  {
+    title: 'UPLOAD LOGIN',
+    path: '',
+    pathMatch: "full",
+    loadComponent: () => import('./features/dashboard/dashboard.component').then(m => m.DashboardComponent)
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./features/uploads/route'),
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+]
+
+bootstrapApplication(AppComponent, { providers: [provideRouter(routes), provideAnimations(), provideAnimations()] }).catch((err) => console.error(err));
